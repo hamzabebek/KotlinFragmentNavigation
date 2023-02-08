f08c4280773593730038328dd3c944027c922ec8
Hamza <111977132+imquaredrs@users.noreply.github.com>
2023-02-08 16:22:00 +0300

    Navigation Framework

---
+++ app/src/main/java/com/example/navigationkotlin/SecondFragment.kt
@@ -0,0 +1,34 @@
+package com.example.navigationkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button2.setOnClickListener{
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment("bebek")
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
